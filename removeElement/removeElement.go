3b2d207650aace7ddcf26833c1d4db4558559965
Leet Oss <ossleet91@gmail.com>
2020-09-10 06:42:28 -0700

    More solutions to remove element problem
    https://leetcode.com/problems/remove-element

--- removeElement/removeElement.go
+++ removeElement/removeElement.go
@@ -22,19 +22,8 @@ package main
 //
 // https://leetcode.com/problems/remove-element/
 
-func swap(nums []int, fwd, bck int) int {
-	for nums[fwd] == nums[bck] && bck > fwd {
-		bck--
-	}
-
-	tmp := nums[bck]
-	nums[bck] = nums[fwd]
-	nums[fwd] = tmp
-
-	return bck
-}
-
-func countOccurences(nums []int, val int) int {
+// Count the number of times 'val' appears in 'nums'.
+func countOf(nums []int, val int) int {
 	count := 0
 
 	for _, n := range nums {
@@ -46,8 +35,28 @@ func countOccurences(nums []int, val int) int {
 	return count
 }
 
+// Remove all occurances of a given element from the array and re-order
+// the remaning elements in-place. Return the length of new array.
+//
+// Use two-pointers approach. Forward pointer scans the whole array for
+// element to be removed. Back pointers is from the back and is used for
+// storing elements that needs to be discarded.
+//
+// Time: O(N). All elements are visited at most once.
+//
+// Space: O(1). Just two pointers and no additional array.
+//
+// nums=[0 1 2 2 3 0 4 2] val=2
+//   fwd/n[f]=0/0 bck/n[b]=7/2 nums=[0 1 2 2 3 0 4 2]
+//   fwd/n[f]=1/1 bck/n[b]=7/2 nums=[0 1 2 2 3 0 4 2] // n[f]==val; swap with n[b] after finding correct b in next step
+//   fwd/n[f]=2/4 bck/n[b]=6/2 nums=[0 1 4 2 3 0 2 2] // b decremented to 6 as n[b]==val and swapped with f
+//   fwd/n[f]=3/0 bck/n[b]=5/2 nums=[0 1 4 0 3 2 2 2] // same as above
+//   fwd/n[f]=4/3 bck/n[b]=5/2 nums=[0 1 4 0 3 2 2 2]
+//   fwd/n[f]=5/2 bck/n[b]=5/2 nums=[0 1 4 0 3 2 2 2]
+//   fwd/n[f]=6/2 bck/n[b]=5/2 nums=[0 1 4 0 3 2 2 2]
+//   fwd/n[f]=7/2 bck/n[b]=5/2 nums=[0 1 4 0 3 2 2 2] // done; b points to final array with all val removed.
 func removeElement(nums []int, val int) int {
-	valCount := countOccurences(nums, val)
+	valCount := countOf(nums, val)
 	if valCount == 0 {
 		return len(nums)
 	} else if valCount == len(nums) {
@@ -56,12 +65,92 @@ func removeElement(nums []int, val int) int {
 
 	bck := len(nums) - 1
 	for fwd := 0; fwd < len(nums); fwd++ {
+		// Find the first position from back whose value isn't
+		// same as what needs to be removed.
+		//
+		// Then, swap it with forward's value which needs to be
+		// removed.
+		//
+		// This guarantees all elements from back till the end
+		// can be discarded.
 		if nums[fwd] == val {
-			bck = swap(nums, fwd, bck)
+			for nums[fwd] == nums[bck] && bck > fwd {
+				bck--
+			}
+			nums[fwd], nums[bck] = nums[bck], nums[fwd]
 		}
 	}
 
 	return bck
+}
+
+// Remove all occurances of a given element from the array and re-order
+// the remaning elements in-place. Return the length of new array.
+//
+// Use two-pointers approach. Fast and slow starts at 0. Fast increments
+// at every iteration. If 'nums[fast] != val', copy slow to fast and
+// increment slow. Do not increment slow when fast points to 'val'. This
+// guarantees slow values (the ones that needs to be removed) will be
+// copied over with fast values. At the end, slow points to end of all
+// 'val' removed array.
+//
+// Time: O(N). All elements are visited at most once.
+//
+// Space: O(1). Just two pointers and no additional array.
+//
+// nums=[]int{0, 1, 2, 2, 3, 0, 4, 2} val=2
+//   slow=1 fast=0 nums=[]int{0, 1, 2, 2, 3, 0, 4, 2}
+//   slow=2 fast=1 nums=[]int{0, 1, 2, 2, 3, 0, 4, 2}
+//   slow=2 fast=2 nums=[]int{0, 1, 2, 2, 3, 0, 4, 2} // s remains at 2 since nums[2]==val
+//   slow=2 fast=3 nums=[]int{0, 1, 2, 2, 3, 0, 4, 2} // s remains at 2 since nums[2]==val
+//   slow=3 fast=4 nums=[]int{0, 1, 3, 2, 3, 0, 4, 2} // f moved to non-val and we copy f to s
+//   slow=4 fast=5 nums=[]int{0, 1, 3, 0, 3, 0, 4, 2}
+//   slow=5 fast=6 nums=[]int{0, 1, 3, 0, 4, 0, 4, 2}
+//   slow=5 fast=7 nums=[]int{0, 1, 3, 0, 4, 0, 4, 2} // done; s points to len of final array.
+func removeElement1(nums []int, val int) int {
+	fast, slow := 0, 0
+	for ; fast < len(nums); fast++ {
+		if nums[fast] != val {
+			nums[slow] = nums[fast]
+			slow++
+		}
+	}
+
+	return slow
+}
+
+// Remove all occurances of a given element from the array and re-order
+// the remaning elements in-place. Return the length of new array.
+//
+// Use two-pointers approach. i and n starts at 0 and len(nums). When
+// n[i] == val, swap it with n-1 and decrement back pointer. Otherwise,
+// increment n. This works as we don't have to maintain order in the
+// resulting array.
+//
+// Time: O(N). All elements are visited at most once.
+//
+// Space: O(1). Just two pointers and no additional array.
+// nums=[0 1 2 2 3 0 4 2] val=2
+//   i/n[i]=1/1 n/n[n-1]=8/2 nums=[0 1 2 2 3 0 4 2]
+//   i/n[i]=2/2 n/n[n-1]=8/2 nums=[0 1 2 2 3 0 4 2] // swap i & n in next step; decrement n
+//   i/n[i]=2/2 n/n[n-1]=7/4 nums=[0 1 2 2 3 0 4 2] // swap i & n in next step; decrement n
+//   i/n[i]=2/4 n/n[n-1]=6/0 nums=[0 1 4 2 3 0 4 2]
+//   i/n[i]=3/2 n/n[n-1]=6/0 nums=[0 1 4 2 3 0 4 2] // swap i & n in next step; decrement n
+//   i/n[i]=3/0 n/n[n-1]=5/3 nums=[0 1 4 0 3 0 4 2]
+//   i/n[i]=4/3 n/n[n-1]=5/3 nums=[0 1 4 0 3 0 4 2]
+//   i/n[i]=5/0 n/n[n-1]=5/3 nums=[0 1 4 0 3 0 4 2] // done; n points to length of final array.
+func removeElement2(nums []int, val int) int {
+	i, n := 0, len(nums)
+	for i < n {
+		if nums[i] == val {
+			nums[i] = nums[n-1]
+			n--
+		} else {
+			i++
+		}
+	}
+
+	return n
 }
 
 func main() {
--- removeElement/removeElement_test.go
+++ removeElement/removeElement_test.go
@@ -1,54 +1,96 @@
 package main
 
 import (
-	"reflect"
 	"testing"
 )
 
-func TestCountOccurences(t *testing.T) {
-	tests := []struct {
-		nums []int
-		val  int
-		want int
-	}{
+type TestCase struct {
+	nums    []int
+	val     int
+	wantLen int
+}
+
+// Returns true if arr contains val.
+func contains(arr []int, val int) bool {
+	for _, n := range arr {
+		if n == val {
+			return true
+		}
+	}
+	return false
+}
+
+func TestRemoveElement(t *testing.T) {
+	tests := []TestCase{
 		{[]int{3, 2, 2, 3}, 3, 2},
-		{[]int{0, 2, 3, 4, 5, 3, 1, 0}, 3, 2},
-		{[]int{1, 1, 1}, 1, 3},
-		{[]int{0, 1, 2, 2, 3, 0, 4, 2}, 2, 3},
-		{[]int{2}, 3, 0},
-		{[]int{3, 3}, 3, 2},
+		{[]int{0, 2, 3, 4, 5, 3, 1, 0}, 3, 6},
+		{[]int{1, 1, 1}, 1, 0},
+		{[]int{0, 1, 2, 2, 3, 0, 4, 2}, 2, 5},
+		{[]int{2}, 3, 1},
+		{[]int{3, 3}, 5, 2},
 	}
 
 	for _, tt := range tests {
-		got := countOccurences(tt.nums, tt.val)
-		if tt.want != got {
-			t.Errorf("failed for %+v: got=%d\n", tt, got)
+		nums := make([]int, len(tt.nums))
+		copy(nums, tt.nums)
+
+		gotLen := removeElement(nums, tt.val)
+		if tt.wantLen != gotLen {
+			t.Errorf("failed for %+v: bad length: gotLen=%d got=%+v\n", tt, gotLen, nums)
+		}
+
+		if contains(nums[:gotLen], tt.val) {
+			t.Errorf("failed for %+v: bad array: gotLen=%d got=%+v\n", tt, gotLen, nums)
 		}
 	}
 }
 
-func TestRemoveElement(t *testing.T) {
-	tests := []struct {
-		nums    []int
-		val     int
-		wantLen int
-		want    []int
-	}{
-		{[]int{3, 2, 2, 3}, 3, 2, []int{2, 2}},
-		{[]int{0, 2, 3, 4, 5, 3, 1, 0}, 3, 6, []int{0, 2, 0, 4, 5, 1}},
-		{[]int{1, 1, 1}, 1, 0, []int{}},
-		{[]int{0, 1, 2, 2, 3, 0, 4, 2}, 2, 5, []int{0, 1, 4, 0, 3}},
-		{[]int{2}, 3, 1, []int{2}},
-		{[]int{3, 3}, 5, 2, []int{3, 3}},
+func TestRemoveElement1(t *testing.T) {
+	tests := []TestCase{
+		{[]int{3, 2, 2, 3}, 3, 2},
+		{[]int{0, 2, 3, 4, 5, 3, 1, 0}, 3, 6},
+		{[]int{1, 1, 1}, 1, 0},
+		{[]int{0, 1, 2, 2, 3, 0, 4, 2}, 2, 5},
+		{[]int{2}, 3, 1},
+		{[]int{3, 3}, 5, 2},
 	}
 
 	for _, tt := range tests {
-		gotLen := removeElement(tt.nums, tt.val)
+		nums := make([]int, len(tt.nums))
+		copy(nums, tt.nums)
+
+		gotLen := removeElement1(nums, tt.val)
 		if tt.wantLen != gotLen {
-			t.Errorf("failed for %+v: gotLen=%d\n", tt, gotLen)
+			t.Errorf("failed for %+v: bad length: gotLen=%d got=%+v\n", tt, gotLen, nums)
 		}
-		if !reflect.DeepEqual(tt.want, tt.nums[:tt.wantLen]) {
-			t.Errorf("failed for %+v\n", tt)
+
+		if contains(nums[:gotLen], tt.val) {
+			t.Errorf("failed for %+v: bad array: gotLen=%d got=%+v\n", tt, gotLen, nums)
+		}
+	}
+}
+
+func TestRemoveElement2(t *testing.T) {
+	tests := []TestCase{
+		{[]int{0, 1, 2, 2, 3, 0, 4, 2}, 2, 5},
+		{[]int{3, 2, 2, 3}, 3, 2},
+		{[]int{0, 2, 3, 4, 5, 3, 1, 0}, 3, 6},
+		{[]int{1, 1, 1}, 1, 0},
+		{[]int{2}, 3, 1},
+		{[]int{3, 3}, 5, 2},
+	}
+
+	for _, tt := range tests {
+		nums := make([]int, len(tt.nums))
+		copy(nums, tt.nums)
+
+		gotLen := removeElement2(nums, tt.val)
+		if tt.wantLen != gotLen {
+			t.Errorf("failed for %+v: bad length: gotLen=%d got=%+v\n", tt, gotLen, nums)
+		}
+
+		if contains(nums[:gotLen], tt.val) {
+			t.Errorf("failed for %+v: bad array: gotLen=%d got=%+v\n", tt, gotLen, nums)
 		}
 	}
 }
